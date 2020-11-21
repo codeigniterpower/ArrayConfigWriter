4e3d68d5c240cbe362b58ad07e9fcded69fbc78d
Wakeel Ogunsanya <wakeel.oguns@gmail.com>
2020-11-21 11:24:56 +0100

    Update test phpunit @cover tag

--- test/config.php
+++ test/config.php
@@ -1,17 +1,13 @@
 <?php 
 
-$config['siteName'] = 'Default Name';
-
-$config['age'] = 52;
-
-$config['address'] = [
-    'line'  => 'Line 1',
-    'city' =>  'New York',
-    'country' => 'USA'
-];
-
-$config['html_element'] = '<span style="color: blue; font-size: 20px;">Hello World</span>';
-
+$config['siteName'] = 'Foo';
+$config['age'] = 20;
+$config['address'] = array (
+  'line' => 'Line One; More Line',
+  'city' => 'Lagos',
+  'country' => 'Nigeria',
+);
+$config['html_element'] = '<div class="color:green">Foo</div>';
 $config['foo'] = [
     'bar'  => 'Foo; Bar',
     'baz' =>  'Foo Baz',
